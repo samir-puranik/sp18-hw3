753ad80064ea01da03f8612ba461c66250081181
Samir Puranik <samir.puranik@berkeley.edu>
2018-02-28 00:14:53 -0800

    Finished the HW

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,15 +1,21 @@
 ## Questions
 
 1. What does the second 'nil' argument in the line 6 text_field_tag of teachers/new.html.erb represent?
+The nil represents the fact that there should be nothing entered in the text box yet.
 
 
 2. Go to `localhost:3000/teachers` in your browser; why does this not work?
+There is no GET route for /teachers yet.
 
 
 3. What type of request did your browser just perform?
+The browser performed a POST request.
 
 
 4. Go back to `localhost:3000/teachers/new`; submit the form again. What URL do you end up at?
+We ended up at the localhost:3000/teachers URL.
 
 
 5. Why does `localhost:3000/teachers` work now?
+Because we are POSTing to /teachers, not GETting from /teachers and there is
+a route already defined for POSTing to /teachers.
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,16 @@
-class TeachersController < ApplicationController
+class StudentsController < ApplicationController
   def new
-    @placeholder_course = '186'
+    @placeholder_name = 'Ex. John Smith'
+    @placeholder_courses = 'Rails Decal, CS 61B, etc.'
+    @placeholder_state = 'California'
   end
 
   def create
     # Hint: params??
-
     # YOUR CODE HERE
     @full_name = params[:full_name]
-    @course = params[:course_name]
-    @grade_level = params[:grade_level]
+    @courses = params[:courses]
+    @state = params[:place_of_origin]
     # This line is explicitly declaring that this controller method should render/serve the
     # 'views/teachers/show.html.erb' view. Otherwise, the controller would try to look for a
     # 'views/teachers/create.html.erb' view file which we do not have.
